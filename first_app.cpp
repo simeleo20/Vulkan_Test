6070ed1870f6e37c7e3671f3945891586b0324b0
simeleo20 <simeleo20@gmail.com>
2024-11-09 17:22:05 +0100

    video 10

--- first_app.cpp
+++ first_app.cpp
@@ -4,6 +4,7 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm.hpp>
+#include <gtc/constants.hpp>
 
 // std
 #include <stdexcept>
@@ -13,13 +14,14 @@ namespace lve
 {
 	struct SimplePushConstantData
 	{
+		glm::mat2 transform{1.f};
 		glm::vec2 offset;
 		alignas(16) glm::vec3 color;
 	};
 
 	FirstApp::FirstApp()
 	{
-		loadModels();
+		loadGameObjects();
 		createPipelineLayout();
 		recreateSwapChain();
 		createCommandBuffers();
@@ -64,7 +66,7 @@ namespace lve
 		}
 	}
 
-	void FirstApp::loadModels()
+	void FirstApp::loadGameObjects()
 	{
 		std::vector<LveModel::Vertex> vertices{
 			{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
@@ -72,7 +74,16 @@ namespace lve
 			{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
 		};
 
-		lveModel = std::make_unique<LveModel>(lveDevice, vertices);
+		auto lveModel = std::make_shared<LveModel>(lveDevice, vertices);
+
+		auto triangle = LveGameObject::createGameObject();
+		triangle.model = lveModel;
+		triangle.color = {0.f, 1.f, 0.f};
+		triangle.transform2d.translation.x = .2f;
+		triangle.transform2d.scale = {2.0f, 0.5f};
+		triangle.transform2d.rotation = .25f * glm::two_pi<float>();
+
+		gameObjects.push_back(std::move(triangle));
 	}
 
 	void FirstApp::createPipelineLayout()
@@ -162,9 +173,6 @@ namespace lve
 
 	void FirstApp::recordCommandBuffer(int imageIndex)
 	{
-		static int frame = 0;
-		frame = (frame + 1) % 1000;
-
 		VkCommandBufferBeginInfo beginInfo{};
 		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -200,24 +208,7 @@ namespace lve
 		vkCmdSetViewport(commandBuffers[imageIndex], 0, 1, &viewport);
 		vkCmdSetScissor(commandBuffers[imageIndex], 0, 1, &scissor);
 
-		lvePipeline->bind(commandBuffers[imageIndex]);
-		lveModel->bind(commandBuffers[imageIndex]);
-
-		for (int j = 0; j < 4; j++)
-		{
-			SimplePushConstantData push{};
-			push.offset = {-0.5f + frame * 0.002f, -0.4f + j * 0.25f};
-			push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
-
-			vkCmdPushConstants(
-				commandBuffers[imageIndex],
-				pipelineLayout,
-				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
-				0,
-				sizeof(SimplePushConstantData),
-				&push);
-			lveModel->draw(commandBuffers[imageIndex]);
-		}
+		renderGameObjects(commandBuffers[imageIndex]);
 
 		vkCmdEndRenderPass(commandBuffers[imageIndex]);
 		if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS)
@@ -225,7 +216,36 @@ namespace lve
 			throw std::runtime_error("failed to record command buffer!");
 		}
 	}
+	void FirstApp::renderGameObjects(VkCommandBuffer commandBuffer)
+	{
 
+		int i = 0;
+		for (auto &obj : gameObjects)
+		{
+			i += 1;
+			obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.001f, glm::two_pi<float>());
+		}
+
+		lvePipeline->bind(commandBuffer);
+
+		for (auto &obj : gameObjects)
+		{
+			SimplePushConstantData push{};
+			push.offset = obj.transform2d.translation;
+			push.color = obj.color;
+			push.transform = obj.transform2d.mat2();
+
+			vkCmdPushConstants(
+				commandBuffer,
+				pipelineLayout,
+				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+				0,
+				sizeof(SimplePushConstantData),
+				&push);
+			obj.model->bind(commandBuffer);
+			obj.model->draw(commandBuffer);
+		}
+	}
 	void FirstApp::drawFrame()
 	{
 		uint32_t imageIndex;
--- first_app.hpp
+++ first_app.hpp
@@ -4,7 +4,7 @@
 #include "lve_window.hpp"
 #include "lve_device.hpp"
 #include "lve_swap_chain.hpp"
-#include "lve_model.hpp"
+#include "lve_game_object.hpp"
 
 // std
 #include <memory>
@@ -27,7 +27,7 @@ namespace lve
 		void run();
 
 	private:
-		void loadModels();
+		void loadGameObjects();
 		void createPipelineLayout();
 		void createPipeline();
 		void createCommandBuffers();
@@ -35,6 +35,7 @@ namespace lve
 		void drawFrame();
 		void recreateSwapChain();
 		void recordCommandBuffer(int imageIndex);
+		void renderGameObjects(VkCommandBuffer commandBuffer);
 
 		LveWindow lveWindow{WIDTH, HEIGHT, "hello vulkan!"};
 		LveDevice lveDevice{lveWindow};
@@ -42,6 +43,6 @@ namespace lve
 		std::unique_ptr<LvePipeline> lvePipeline;
 		VkPipelineLayout pipelineLayout;
 		std::vector<VkCommandBuffer> commandBuffers;
-		std::unique_ptr<LveModel> lveModel;
+		std::vector<LveGameObject> gameObjects;
 	};
 }
